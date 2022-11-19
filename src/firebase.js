4385b54ca6a314be6b1e52caaafda1b73029f753
artofnuel <artofnuel@gmail.com>
2022-11-19 14:23:00 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
-    appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyAGZtr1sRLeCYzBfQ3Gw6BlGIXZ7pi7XYQ",
+    authDomain: "todo-app-aon.firebaseapp.com",
+    projectId: "todo-app-aon",
+    storageBucket: "todo-app-aon.appspot.com",
+    messagingSenderId: "602794448802",
+    appId: "1:602794448802:web:ddd739cf49c76fba7244fb"
 };
 
 // Initialize Firebase
